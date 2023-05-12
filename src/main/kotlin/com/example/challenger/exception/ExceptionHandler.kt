17784528460dca9a726c793452796f77c3d86fb8
syl-minjae <bapbodanbbang@likelion.net>
2023-05-12 13:23:24 +0900

    setting: ktlin"

--- src/main/kotlin/com/example/challenger/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/example/challenger/exception/ExceptionHandler.kt
@@ -1,17 +1,15 @@
 package com.example.challenger.exception
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
-import java.lang.Exception
 
 @ControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(BaseException::class)
     @ResponseBody
-    fun handleException(exception: BaseException):ResponseEntity<ErrorResponse> {
+    fun handleException(exception: BaseException): ResponseEntity<ErrorResponse> {
         return ResponseEntity.status(exception.statusCode).body(
             ErrorResponse(
                 statusCode = exception.statusCode,
